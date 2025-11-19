e7d63bcf943336e1daca21b53ec474ed00a4466a
d8zval <valentina.ochoa_her@uao.edu.co>
2025-11-18 21:15:57 -0500

    script puntos funcional

--- Assets/Scripts/ScoreByTrigger.cs
+++ Assets/Scripts/ScoreByTrigger.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class ScoreByTrigger : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class ScoreByTrigger : MonoBehaviour
     [Header("Puntos")]
     public int score = 0;
 
+    // Utilizamos un HashSet para evitar contar las mismas colisiones múltiples veces
+    private HashSet<Collider> objetosContabilizados = new HashSet<Collider>();
+
     private void Start()
     {
         UpdateScoreUI();
@@ -16,6 +20,10 @@ public class ScoreByTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // Verificar si el objeto ya ha sido contabilizado
+        if (objetosContabilizados.Contains(other)) return;
+
+        // Si el objeto tiene un tag relevante, le sumamos o restamos puntos
         if (other.CompareTag("roja"))
         {
             score += 10;
@@ -33,6 +41,10 @@ public class ScoreByTrigger : MonoBehaviour
             return; // Si no tiene tag relevante, no hace nada
         }
 
+        // Añadimos el objeto al HashSet para que no sea contado de nuevo
+        objetosContabilizados.Add(other);
+
+        // Actualiza la UI de los puntos
         UpdateScoreUI();
     }
 
